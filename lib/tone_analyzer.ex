087b629c16b9d2bbed3f4624d145edd985ef30e8
yianna <yianna@thoughtbot.com>
2016-12-12 13:10:00 -0500

    - Add functions for getting username and password from environment. Add the url to the api endpoint

--- lib/tone_analyzer.ex
+++ lib/tone_analyzer.ex
@@ -1,5 +1,13 @@
 defmodule ToneAnalyzer do
-  def hello
-    IO."hey yianna"
+  def username do
+    System.get_env("TONE_ANALYZER_USERNAME");
+  end
+
+  def password do
+    System.get_env("TONE_ANALYZER_PASS");
+  end
+  
+  def watsonEndPoint do
+    "https://gateway.watsonplatform.net/tone-analyzer/api/v3";
   end
 end
