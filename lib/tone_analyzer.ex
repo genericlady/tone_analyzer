454ec63c9f9a4d42a9c8f616d4b3768424a25eef
yianna <yianna@thoughtbot.com>
2016-12-13 09:11:10 -0500

    Add functions for get and post with ibm watson api endpoint

--- lib/tone_analyzer.ex
+++ lib/tone_analyzer.ex
@@ -1,13 +1,39 @@
 defmodule ToneAnalyzer do
+  def url do
+    "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19"
+  end
+
   def username do
-    System.get_env("TONE_ANALYZER_USERNAME");
+    System.get_env("TONE_ANALYZER_USERNAME")
   end
 
   def password do
-    System.get_env("TONE_ANALYZER_PASS");
+    System.get_env("TONE_ANALYZER_PASS")
+  end
+
+end
+
+defmodule ServiceCall do
+  def analyzeText(textInput) do
+    "&text=" <> textInput
   end
   
-  def watsonEndPoint do
-    "https://gateway.watsonplatform.net/tone-analyzer/api/v3";
+  def get do
+    HTTPotion.get url, [basic_auth: authOptions]
   end
+
+  defp post do
+    HTTPotion.post url, [
+      body: "text=" <> URI.encode_www_form("w o r l d !!"),
+      headers: [
+        "User-Agent": "ToneAnalyzer",
+        "Content-Type": "application/json"
+      ]
+    ]
+  end
+
+  defp authOptions do
+    {ToneAnalyzer.username, ToneAnalyzer.password}
+  end
+
 end
