55d7e31e7c93407da440e277f0ccec26d48f128b
yianna <yianna@thoughtbot.com>
2016-12-17 09:42:31 -0500

    Move ServiceCall into a folder named lib/services

---
+++ lib/services/service_call.ex
@@ -0,0 +1,52 @@
+defmodule ServiceCall do
+  import ToneAnalyzer
+  import String, only: [replace: 3]
+
+  def start(:get, text) do
+    analyze_text(:get, text)
+      |> to_struct
+      |> document_tone
+      |> tone_categories
+  end
+
+  def start(:post, text) do
+    analyze_text(:post, text)
+      |> to_struct
+  end
+
+  defp document_tone(response_body) do
+    response_body["document_tone"]
+  end
+
+  defp tone_categories(document_tone) do
+    document_tone["tone_categories"]
+  end
+
+  defp to_struct(response) do
+    Poison.decode!(response.body)
+  end
+
+  defp analyze_text(:get, text) do
+    HTTPotion.get url <> "&text=" <> encode(text), [basic_auth: login_credentials]
+  end
+
+  defp analyze_text(:post, text) do
+    HTTPotion.post url, [
+      body: "&text=#{text}",
+      headers: [
+        "User-Agent": "ToneAnalyzer",
+        "Content-Type": "application/json"
+      ],
+      basic_auth: login_credentials
+    ]
+  end
+
+  defp encode(text) do
+    replace text, " ", "%20"
+  end
+ 
+  defp login_credentials do
+    {username, password}
+  end
+
+end
