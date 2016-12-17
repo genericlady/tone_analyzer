017cf2925f91470e9d7f358dc6c3af1cdaee9032
yianna <yianna@thoughtbot.com>
2016-12-17 12:18:44 -0500

    Abstract some url building from tone_analyzer.ex into url_builder.ex

--- lib/models/url_builder.ex
+++ lib/models/url_builder.ex
@@ -1,17 +1,29 @@
 defmodule UrlBuilder do
   import System, only: [get_env: 1]
+	import URI, only: [encode: 1]
+
+	def build_url(text) do
+		url <> encode(text)
+	end
 
   def url do
-    "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19"
+    "https://#{host}#{path}?version=2016-05-19&text="
   end
 
-  def username do
+  def credentials(:username) do
     get_env("TONE_ANALYZER_USERNAME")
   end
 
-  def password do
+  def credentials(:password) do
     get_env("TONE_ANALYZER_PASS")
   end
 
+	defp host do
+		"gateway.watsonplatform.net"
+	end
+
+	defp path do
+		"/tone-analyzer/api/v3/tone"
+	end
 end
 
