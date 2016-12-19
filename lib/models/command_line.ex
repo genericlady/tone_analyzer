193f456548ba5225ae8d3d341823548e48a8e11b
genericlady <yiannakokalas@gmail.com>
2016-12-18 20:41:08 -0500

    Add function to print table in the command line module in lib/models
    Add behavior for printing the data into three tables. One for each category, Emotion, Language and Social.
    
    Add instructions on compiling and using the command line application
    
    Abstract behavior from ToneAnalyzer and place in ToneAnalyzer.Commandline. ToneAnalyzer is now just a namespace for containing many modules that work together
    
    Change escript option for main module to ToneAnalyzer.CommandLine
    
    Commit newly compiled executable

---
+++ lib/models/command_line.ex
@@ -0,0 +1,57 @@
+defmodule ToneAnalyzer.CommandLine do
+	import Enum, only: [map: 2, each: 2]
+
+  @moduledoc """
+  This module defines the main run routine used to
+  parse command line arguments and process them.
+  """
+  @vsn "1.0"
+
+  def main(args) do
+    args |> parse_args |> process |> print_each_table
+  end
+
+  def process([]) do
+    IO.puts 'Try appending --text="Analyze Text"'
+  end
+
+  def process(options) do
+    ServiceCall.start(:get, options[:text]) |> to_tables
+  end
+
+  defp parse_args(args) do
+    {options, _, _} = 
+      OptionParser.
+        parse(
+          args,
+          switches: [text: :string]
+        )
+    options
+  end
+
+	def to_tables(categories) do
+		map(categories,
+			fn(category) ->
+				title = category["category_name"]
+				header = ["Tone Name", "Score"]
+				rows = to_rows(category["tones"])
+				TableRex.quick_render!(rows, header, title)
+			end
+		)
+	end
+
+	def print_each_table(tables) do
+		each(tables, &IO.puts(&1))
+  end
+
+	def to_rows(tones) do
+		map(tones,
+			fn(tone) ->
+				[
+					tone["tone_name"],
+					tone["score"]
+				]
+			end
+		)
+	end
+end
--- lib/tone_analyzer.ex
+++ lib/tone_analyzer.ex
@@ -1,25 +1,2 @@
 defmodule ToneAnalyzer do
-
-  def main(args) do
-    args |> parse_args |> process
-  end
-
-  def process([]) do
-    IO.puts 'Try appending --text="Analyze Text"'
-  end
-
-  def process(options) do
-    ServiceCall.start(:get, options[:text])
-  end
-
-  defp parse_args(args) do
-    {options, _, _} = 
-      OptionParser.
-        parse(
-          args,
-          switches: [text: :string]
-        )
-    options
-  end
-
 end
